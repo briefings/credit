5e519879768dfe5b8c07e00292456b634625783e
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-04-19 17:32:32 +0100

    Update config.py

--- config.py
+++ config.py
@@ -12,7 +12,6 @@ class Config:
 
     @staticmethod
     def source():
-
         SourceAttributes = collections.namedtuple(
             typename='SourceAttributes',
             field_names=['url', 'dtype', 'numeric', 'label'])
@@ -51,3 +50,36 @@ class Config:
              ['A61', 'A62', 'A63', 'A64', 'A65', 'A71', 'A72', 'A73', 'A74', 'A75',
               'curr_res_since', 'n_e_credits_this_bank', 'A171', 'A172', 'A173', 'A174'],
              ['curr_res_since', 'A171', 'A172', 'A173', 'A174']))
+
+    @staticmethod
+    def categories():
+        CategoricalData = collections.namedtuple(
+            typename='CategoricalData', field_names=['fields', 'arrays', 'dictionary'])
+
+        fields = ['e_chq_acc_status', 'credit_history', 'purpose', 'savings_acc_class',
+                  'curr_emp_class', 'sex_and_status', 'other_debtors_class', 'property',
+                  'other_i_plans', 'housing', 'job', 'telephone', 'foreign_worker']
+
+        arrays = [np.array(['A11', 'A12', 'A13', 'A14']), np.array(['A30', 'A31', 'A32', 'A33', 'A34']),
+                  np.array(['A40', 'A41', 'A42', 'A43', 'A44', 'A45', 'A46', 'A47', 'A48', 'A49', 'A410']),
+                  np.array(['A61', 'A62', 'A63', 'A64', 'A65']), np.array(['A71', 'A72', 'A73', 'A74', 'A75']),
+                  np.array(['A91', 'A92', 'A93', 'A94', 'A95']), np.array(['A101', 'A102', 'A103']),
+                  np.array(['A121', 'A122', 'A123', 'A124']), np.array(['A141', 'A142', 'A143']),
+                  np.array(['A151', 'A152', 'A153']), np.array(['A171', 'A172', 'A173', 'A174']),
+                  np.array(['A191', 'A192']), np.array(['A201', 'A202'])]
+
+        dictionary = {'e_chq_acc_status': {'A11': 0, 'A12': 1, 'A13': 2, 'A14': 3},
+                      'credit_history': {'A30': 0, 'A31': 1, 'A32': 2, 'A33': 3, 'A34': 4},
+                      'purpose': {'A40': 0, 'A41': 1, 'A42': 2, 'A43': 3, 'A44': 4, 'A45': 5, 'A46': 6, 'A47': 7,
+                                  'A48': 8, 'A49': 9, 'A410': 10},
+                      'savings_acc_class': {'A61': 0, 'A62': 1, 'A63': 2, 'A64': 3, 'A65': 4},
+                      'curr_emp_class': {'A71': 0, 'A72': 1, 'A73': 2, 'A74': 3, 'A75': 4},
+                      'other_debtors_class': {'A101': 0, 'A102': 1, 'A103': 2},
+                      'property': {'A121': 0, 'A122': 1, 'A123': 2, 'A124': 3},
+                      'other_i_plans': {'A141': 0, 'A142': 1, 'A143': 2},
+                      'housing': {'A151': 0, 'A152': 1, 'A153': 2},
+                      'job': {'A171': 0, 'A172': 1, 'A173': 2, 'A174': 3},
+                      'telephone': {'A191': 0, 'A192': 1},
+                      'foreign_worker': {'A201': 1, 'A202': 0}}
+
+        return CategoricalData._make((fields, arrays, dictionary))
