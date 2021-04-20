94b14a4b1abcd0a4fb715d4b2ce979d335811385
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-04-20 17:27:52 +0100

    Update credit/src/modelling.py

--- credit/src/modelling.py
+++ credit/src/modelling.py
@@ -3,19 +3,19 @@ import pandas as pd
 import config
 
 
-class Baseline:
+class Modelling:
 
     def __init__(self):
 
         configurations = config.Config()
 
-        self.instances = configurations.instances()
+        self.modelling = configurations.modelling()
 
     def read_(self):
 
         try:
-            data = pd.read_csv(filepath_or_buffer=self.instances.url, header=0,
-                               encoding='utf-8', dtype=self.instances.dtype)
+            data = pd.read_csv(filepath_or_buffer=self.modelling.url, header=0,
+                               encoding='utf-8', dtype=self.modelling.dtype)
         except OSError as err:
             raise Exception(err.strerror) in err
 
