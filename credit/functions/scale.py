9b72e8f6c707f74fc70e6e54ff105b4e788a0585
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-04-21 22:31:48 +0100

    Update credit/functions/scale.py

--- credit/functions/scale.py
+++ credit/functions/scale.py
@@ -7,30 +7,44 @@ import config
 
 class Scale:
 
-    def __init__(self):
+    def __init__(self, blob: pd.DataFrame):
         """
 
         """
 
+        self.blob = blob
+
+        # From config.py
         self.numeric = config.Config().numeric
 
-    def exc(self, blob: pd.DataFrame) -> (np.ndarray, sklearn.preprocessing.StandardScaler):
+        # The numeric & categorical fields
+        self.numerical = list(set(self.numeric).intersection(set(blob.columns)))
+        self.categorical = list(set(blob.columns).difference(set(self.numeric)))
+
+    def pieces(self, scaler) -> np.ndarray:
         """
 
-        :param blob:
+        :param scaler:
         :return:
         """
 
-        # The numeric & categorical fields
-        numerical = list(set(self.numeric).intersection(set(blob.columns)))
-        categorical = list(set(blob.columns).difference(set(self.numeric)))
+        # Scale
+        scaled_ = scaler.transform(X=self.blob[self.numerical].values)
+
+        # Altogether
+        return np.concatenate((scaled_, self.blob[self.categorical].values), axis=1)
+
+    def exc(self) -> (np.ndarray, sklearn.preprocessing.StandardScaler):
+        """
+
+        :return:
+        """
 
         # Scaling the numeric fields only
         scaler = sklearn.preprocessing.StandardScaler(with_mean=False)
-        scaler.fit(X=blob[numerical].values)
-        scaled_ = scaler.transform(X=blob[numerical].values)
+        scaler.fit(X=self.blob[self.numerical].values)
 
-        # Altogether
-        data = np.concatenate((scaled_, blob[categorical].values), axis=1)
+        # Use scaler to scale the numerical data, subsequently reconstruct the data
+        data = self.pieces(scaler=scaler)
 
         return data, scaler
