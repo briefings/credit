d9b35bce5a3832dab05dd38f1c68882f2e4693f2
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-04-29 22:38:29 +0100

    Update scripts.sh

--- scripts.sh
+++ scripts.sh
@@ -7,11 +7,14 @@
 # apt-get install ghostscript &>> tex.log
 # apt-get install dvipng &>> tex.log
 
+
 # logs
 rm -rf *.log
 
+
 # config
 rm -rf config.py
+
 
 # Packages
 mkdir logs
@@ -20,9 +23,16 @@ pip install pymc3==3.11.2 &> logs/pymc3.log
 pip install cloudpickle==1.6.0 &> logs/cloudpickle.log
 pip install dask[complete]==2.30.0 &> logs/dask.log
 
+
 # https://linux.die.net/man/1/wget
 wget -q https://github.com/briefings/credit/raw/develop/credit.zip
 wget -q https://raw.githubusercontent.com/briefings/credit/develop/config.py
+
+wget -q -P warehouse https://github.com/briefings/credit/raw/develop/warehouse/baseline.pkl
+wget -q -P warehouse https://github.com/briefings/credit/raw/develop/warehouse/model.pkl
+wget -q -P warehouse https://github.com/briefings/credit/raw/develop/warehouse/trace.zip
+wget -q -P warehouse https://raw.githubusercontent.com/briefings/credit/develop/data/testing.csv
+
 
 # https://linux.die.net/man/1/unzip
 rm -rf credit
