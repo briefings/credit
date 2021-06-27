1b91b6e7a13407b245581ec0f7aa4cc837a1845f
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-06-27 01:49:47 +0100

    Update risk/functions/sample.py

--- risk/functions/sample.py
+++ risk/functions/sample.py
@@ -1,61 +1,30 @@
-import collections
-
-import numpy as np
+import imblearn
 import pandas as pd
-import sklearn.utils
+
+import config
 
 
 class Sample:
 
-    def __init__(self, frame: pd.DataFrame, strata: list, sampling: collections.namedtuple):
+    def __init__(self):
+        """
+        Constructor
         """
 
-        :param frame:
-        :param strata: The field/s of frame that will guide the sampling
-        :param sampling: Wherein
-                            sampling.n_samples: the number of instances per distinct strata values combination
-                            sampling.random_state: ...
+        configurations = config.Config()
+        self.SEED = configurations.SEED
+
+    def exc(self, blob: pd.DataFrame, target: str):
         """
 
-        self.frame = frame
-        self.strata = strata
-        self.sampling = sampling
-
-    def sample(self, vector: np.ndarray) -> pd.DataFrame:
-        """
-
-        :param vector:
+        :param blob: The data set that will be sampled
+        :param target: The labels field
         :return:
         """
 
-        indices = sklearn.utils.shuffle(vector)
-        indices = sklearn.utils.shuffle(indices)
+        # Over-sampling via SVNSMOTE
+        # https://imbalanced-learn.org/stable/references/generated/imblearn.over_sampling.SVMSMOTE.html
+        svnsmote = imblearn.over_sampling.SVMSMOTE(random_state=self.SEED, sampling_strategy='all', k_neighbors=9)
+        x_training_resampled, y_training_resampled = svnsmote.fit_resample(X=blob.drop(columns=target), y=blob[target])
 
-        rebuilt = self.frame.iloc[indices, :]
-        rebuilt.reset_index(drop=True, inplace=True)
-
-        return rebuilt
-
-    def exc(self) -> pd.DataFrame:
-        """
-
-        :return:
-        """
-
-        permutations = self.frame[self.strata].to_numpy()
-        combinations = np.unique(permutations, axis=0)
-
-        vector = np.array([])
-        for combination in combinations:
-            states = (permutations == combination)
-            conditions = np.all(states, axis=1)
-
-            indices = self.frame[conditions].index
-            array_ = indices.to_numpy()
-
-            select = sklearn.utils.resample(
-                array_, replace=True, n_samples=self.sampling.n_samples, random_state=self.sampling.random_state)
-
-            vector = np.concatenate((vector, select))
-
-        return self.sample(vector=vector)
+        return pd.concat((x_training_resampled, y_training_resampled), axis=1, ignore_index=False)
