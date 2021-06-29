d6ade7877b375f6ec6dee3d4087eb152c0e2b776
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-06-29 19:50:01 +0100

    Update scripts.sh

--- scripts.sh
+++ scripts.sh
@@ -7,12 +7,10 @@
 # apt-get install ghostscript &>> tex.log
 # apt-get install dvipng &>> tex.log
 
-
 # Setting-up
 rm -rf log
 rm -rf config.py
 mkdir logs
-
 
 # Packages
 pip install yellowbrick==1.3.post1 &> logs/yellow.log
@@ -20,11 +18,9 @@ pip install pymc3==3.11.2 &> logs/pymc3.log
 pip install cloudpickle==1.6.0 &> logs/cloudpickle.log
 pip install dask[complete]==2.30.0 &> logs/dask.log
 
-
 # https://linux.die.net/man/1/wget
 wget -q https://github.com/briefings/risk/raw/develop/risk.zip
 wget -q https://raw.githubusercontent.com/briefings/risk/develop/config.py
-
 
 # https://linux.die.net/man/1/unzip
 rm -rf risk
