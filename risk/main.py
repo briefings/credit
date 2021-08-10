3355b10dc237952fa913c5777dbd8e931cbf3d74
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-08-10 20:38:34 +0100

    Update risk/main.py

--- risk/main.py
+++ risk/main.py
@@ -8,46 +8,61 @@ def main():
 
     url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/statlog/german/german.data'
 
+    # The original data
     data = risk.io.archetype.Archetype(). \
         exc(url=url, filename='risk.csv')
     logger.info(data.info())
 
+    # Address inconsistencies
     baseline = risk.io.baseline.Baseline(frame=data).exc()
-    risk.io.template.baseline.Baseline().exc()
+    risk.architectures.baseline.Baseline().exc()
     logger.info(baseline.info())
 
+    # Prepare for modelling: Textual Categorical Fields > One Hot Encodings OR Binary
     modelling = risk.io.modelling.Modelling(frame=baseline).exc()
-    risk.io.template.modelling.Modelling().exc()
+    properties, _ = risk.architectures.modelling.Modelling().exc()
     logger.info(modelling.info())
 
+    # Split into training/testing sets
     SplittingArguments = collections.namedtuple(
         typename='SplittingArguments', field_names=['train_size', 'random_state', 'target', 'strata'])
     training = risk.functions.split.Split(arguments=SplittingArguments._make(
         (0.65, configurations.SEED, configurations.target, ['female', configurations.target]))).exc(data=modelling)
     logger.info(training.info())
 
+    # Get embeddings
+    dataframe = risk.embeddings.interface.Interface(
+        training=training, properties=properties).exc()
+    logger.info(dataframe.info())
+
+    # Model
+
 
 if __name__ == '__main__':
 
+    # Paths
     root = os.getcwd()
     sys.path.append(root)
     sys.path.append(os.path.join(root, 'risk'))
 
+    # Logging
     logging.basicConfig(level=logging.INFO, format='%(message)s\n%(asctime)s.%(msecs)03d', datefmt='%Y-%m-%d %H:%M:%S')
     logger = logging.getLogger(__name__)
 
+    # Classes
     import config
 
     import risk.io.archetype
     import risk.io.baseline
     import risk.io.definitions
     import risk.io.modelling
-
-    import risk.io.template.baseline
-    import risk.io.template.modelling
+    import risk.architectures.baseline
+    import risk.architectures.modelling
 
     import risk.functions.split
+    import risk.embeddings.interface
 
+    # Instantiating
     risk.io.definitions.Definitions().exc()
 
     configurations = config.Config()
