555a89b42a69843b8427b8d4a80ece0b4b6952e6
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-08-10 20:37:12 +0100

    Add risk/embeddings/interface.py

---
+++ risk/embeddings/interface.py
@@ -0,0 +1,75 @@
+import os
+import json
+
+import numpy as np
+import pandas as pd
+
+import risk.embeddings.representations
+
+import config
+
+
+class Interface:
+
+    def __init__(self, training: pd.DataFrame, properties: dict):
+        """
+
+        :param training:
+        :param properties:
+        """
+
+        self.training = training
+        self.properties = properties
+
+        self.configurations = config.Config()
+        self.path = os.path.join(self.configurations.warehouse, 'representations')
+
+    def __restructure(self, embedded, mappings) -> pd.DataFrame:
+        
+        numeric = self.training[self.properties['numeric']]
+        binary = self.training[self.properties['binary']]
+        target = self.training[self.properties['target']]
+        polytomous = self.training[list(mappings.keys())]
+
+        return pd.concat((numeric, embedded, binary, target, polytomous),
+                         axis=1, ignore_index=False)
+        
+    @staticmethod
+    def __decompose(representations) -> (pd.DataFrame, dict):
+
+        # The embedded forms of the polytomous categorical fields
+        frames = [representations[i][0] for i in np.arange(len(representations))]
+        embedded = pd.concat(frames, axis=1, ignore_index=False)
+
+        # The 'polytomous categorical fields' & 'T SNE Representation' mappings
+        mappings = {representations[i][2]: representations[i][1] for i in np.arange(len(representations))}
+        
+        return embedded, mappings
+
+    def __write(self, data: pd.DataFrame):
+
+        try:
+            data.to_csv(path_or_buf=os.path.join(self.path, 'data.csv'),
+                        header=True, index=False, encoding='utf-8')
+            return True
+        except OSError as err:
+            raise Exception(err.strerror)
+
+    def exc(self):
+
+        # Embeddings, and the associated mappings
+        groups = self.properties['polytomous']
+        representations = risk.embeddings.representations.Representations(blob=self.training).\
+            exc(groups=groups)
+        embedded, mappings = self.__decompose(representations=representations)
+
+        # Next, include the DataFrame of embeddings in the training data set
+        data = self.__restructure(embedded=embedded, mappings=mappings)
+
+        # Write
+        if self.__write(data=data):
+
+            with open(os.path.join(self.path, 'mappings.json'), 'w') as disk:
+                json.dump(mappings, disk)
+
+            return data
