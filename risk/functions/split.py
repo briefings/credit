98ec123377ec865ccf08086708706a4cfbdc2cf6
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-08-11 12:17:28 +0100

    Update risk/functions/split.py

--- risk/functions/split.py
+++ risk/functions/split.py
@@ -35,7 +35,7 @@ class Split:
         testing.to_csv(path_or_buf=os.path.join(self.path, 'testing.csv'),
                        header=True, index=False, encoding='UTF-8')
 
-    def exc(self, data: pd.DataFrame) -> pd.DataFrame:
+    def exc(self, data: pd.DataFrame) -> (pd.DataFrame, pd.DataFrame):
         """
 
         :param data:
@@ -56,4 +56,4 @@ class Split:
         
         self.__write(training=training, testing=testing)
 
-        return training
+        return training, testing
